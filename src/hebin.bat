aa2b2574d3f00e4cf9c81d32e187ea80558ef0e7
QAIU <73622400@qq.com>
2021-10-30 03:47:04 +0800

    优化合并方法

--- src/hebin.bat
+++ src/hebin.bat
@@ -1,9 +1,9 @@
 
-cscript jsZip.js ..\sxwnl.htm index.htm tools.js+eph0.js+ephB.js+eph.js+JW.js+lunar.js+vml.js+help.js+page_gj.js 1000000 y
+cscript jsZip.js ..\index.htm index.htm tools.js+eph0.js+ephB.js+eph.js+JW.js+lunar.js+vml.js+help.js+page_gj.js 1000000 y
 
-cscript jsZip.js ..\sxwnlmp.htm indexmp.htm tools.js+eph0.js+ephB.js+eph.js+JW.js+lunar.js+vml.js+help.js+page_gj.js 1000000 y
+cscript jsZip.js ..\indexmp.htm indexmp.htm tools.js+eph0.js+ephB.js+eph.js+JW.js+lunar.js+vml.js+help.js+page_gj.js 1000000 y
 
-..\convertcp.exe 936 65001 /i ..\sxwnl.htm /o ..\sxwnl
-move ..\sxwnl ..\sxwnl.htm > nil
-..\convertcp.exe 936 65001 /i ..\sxwnlmp.htm /o ..\sxwnlmp
-move ..\sxwnlmp ..\sxwnlmp.htm > nil
+..\convertcp.exe 936 65001 /i ..\index.htm /o ..\index
+move ..\index ..\index.htm > nil
+..\convertcp.exe 936 65001 /i ..\indexmp.htm /o ..\indexmp
+move ..\indexmp ..\indexmp.htm > nil
