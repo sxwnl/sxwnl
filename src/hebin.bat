2e4540d389d28e0aa5fcfe31fb0c4a9acf89aa7b
QAIU <736226400@qq.com>
2022-08-10 10:43:39 +0800

    所有源文件使用windows换行符(CRLF)

--- src/hebin.bat
+++ src/hebin.bat
@@ -4,6 +4,6 @@ cscript jsZip.js ..\index.htm index.htm tools.js+eph0.js+ephB.js+eph.js+JW.js+lu
 cscript jsZip.js ..\indexmp.htm indexmp.htm tools.js+eph0.js+ephB.js+eph.js+JW.js+lunar.js+vml.js+help.js+page_gj.js 1000000 y
 
 ..\convertcp.exe 936 65001 /i ..\index.htm /o ..\index
-move ..\index ..\index.htm > nil
+move ..\index ..\index.htm > nul
 ..\convertcp.exe 936 65001 /i ..\indexmp.htm /o ..\indexmp
-move ..\indexmp ..\indexmp.htm > nil
+move ..\indexmp ..\indexmp.htm > nul
